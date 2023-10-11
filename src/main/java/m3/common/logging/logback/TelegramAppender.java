cc2c804dec66df02c888ec0da169b57f83d20c36
a.f.larionov <a.f.larionov@gmail.com>
2023-10-11 17:56:13 +0300

    - add alerter functinal - rename m3.users.* -> m3.commons.*

--- src/main/java/m3/common/logging/logback/TelegramAppender.java
+++ src/main/java/m3/common/logging/logback/TelegramAppender.java
@@ -39,7 +39,7 @@ public class TelegramAppender extends AppenderBase<ILoggingEvent> {
                 "?chat_id=" + chatId +
                 "&text=" + URLEncoder.encode(encodedString, StandardCharsets.UTF_8);
 
-        endpoint = endpoint.substring(0, Math.min(endpoint.length(),1024));
+        endpoint = endpoint.substring(0, Math.min(endpoint.length(), 1024));
 
         ProcessBuilder pb = new ProcessBuilder();
         pb.command("curl", endpoint, "--ssl-no-revoke");
@@ -51,43 +51,4 @@ public class TelegramAppender extends AppenderBase<ILoggingEvent> {
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-
-//        var chatId = "-1001416055228";
-////1194574646:AAEr6xf1nNEhveh-N5Drv3KOQ8I9UWrTkZw
-//        var endpoint = "https://api.telegram.org/bot"
-//                + token
-//                + "/sendMessage" +
-//                "?chat_id=" + chatId +
-//                "&text=" + URLEncoder.encode(message);
-//        System.out.println(endpoint);
-//
-////        ProcessBuilder pb = new ProcessBuilder();
-////        var url = "https://api.telegram.org/bot1194574646:AAEr6xf1nNEhveh-N5Drv3KOQ8I9UWrTkZw/sendMessage?chat_id=-1001416055228&text=Starting";
-////        url = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatId + "&text=" + URLEncoder.encode(message);
-////        pb.command("curl",   url , "--ssl-no-revoke");
-////        Process p = pb.start();
-////        p.waitFor();
-////        new String(p.getErrorStream().readAllBytes(), StandardCharsets.UTF_8) +
-////                new String(p.getInputStream().readAllBytes(), StandardCharsets.UTF_8) +
-////                "-";
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        processBuilder.command("curl", endpoint, "--ssl-no-revoke");
-//        try {
-//
-//            Process process = processBuilder.start();
-//            process.waitFor();
-//            1
-//            process.destroy();
-//            System.out.println("telesend: " + process.exitValue());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
     }
-
-    public Encoder<ILoggingEvent> getEncoder() {
-        return encoder;
-    }
-}
