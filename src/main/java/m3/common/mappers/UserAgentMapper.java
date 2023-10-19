54f6c3bb0630d35b8eaa785a38e22207093b541c
a.f.larionov <a.f.larionov@gmail.com>
2023-10-20 01:55:22 +0300

    - update Dockerfile

--- src/main/java/m3/common/mappers/UserAgentMapper.java
+++ src/main/java/m3/common/mappers/UserAgentMapper.java
@@ -2,9 +2,11 @@ package m3.common.mappers;
 
 import m3.common.entities.UserAgentEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserAgentMapper {
 
+    @Mapping(target = "id", ignore = true)
     UserAgentEntity toEntity(Long uid, String agent);
 }
